b84a7484a3f90b790d6f508fc532b229760f3177
Kaustubh <kaustubhkumbhare02@gmail.com>
2024-06-22 00:22:11 +0530

    validation for public key

--- check_balance.ts
+++ check_balance.ts
@@ -1,14 +1,19 @@
 import { Connection, PublicKey, clusterApiUrl, LAMPORTS_PER_SOL } from "@solana/web3.js"
 
 const connection = new Connection(clusterApiUrl("devnet"))
-const publicKey = new PublicKey(process.argv[2]);
+try {
+    const publicKey = new PublicKey(process.argv[2]);
+    const isValidPublicKey = PublicKey.isOnCurve(publicKey.toBytes())
 
-if (!publicKey) {
-    console.log("Public key is required")
+    if (isValidPublicKey) {
+        const balance: any = await connection.getBalance(publicKey);
+        const balanceInSol: any = balance / LAMPORTS_PER_SOL;
+
+        console.log(`The balance of account having public key : ${publicKey} is ${balance} in lamports`)
+        console.log(`The balance of account having public key : ${publicKey} is ${balanceInSol} in sol`)
+    } else {
+        console.log("Invalid public key")
+    }
+} catch (error) {
+    console.log("Invalid public key input")
 }
-
-const balance: any = await connection.getBalance(publicKey);
-const balanceInSol: any = balance / LAMPORTS_PER_SOL;
-
-console.log(`The balance of account having public key : ${publicKey} is ${balance} in lamports`)
-console.log(`The balance of account having public key : ${publicKey} is ${balanceInSol} in sol`)
