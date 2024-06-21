ddb908fe0a93e3e10ee94d8dd2d43599fdcc092f
Kaustubh <kaustubhkumbhare02@gmail.com>
2024-06-21 22:20:54 +0530

    reading_data_from_sol_blockchain

--- generate-keypair.ts
+++ generate-keypair.ts
@@ -1,12 +1,18 @@
-import * as dotenv from 'dotenv';
-dotenv.config();
-import { getKeypairFromEnvironment } from "@solana-developers/helpers";
+import { Keypair } from "@solana/web3.js";
+
+const keypair = Keypair.generate();
+
+console.log(`The public key is: `, keypair.publicKey.toBase58());
+console.log(`The secret key is: `, keypair.secretKey);
+console.log(`✅ Finished!`);    
+
+
+// import * as dotenv from 'dotenv';
+// dotenv.config();
+// import { getKeypairFromEnvironment } from "@solana-developers/helpers";
 
 // const keypair = getKeypairFromEnvironment("SECRET_KEY");
-const keypair = getKeypairFromEnvironment("SECRET_KEY");
 
-console.log('keypair', keypair);
-
-console.log(
-    `✅ Finished! We've loaded our secret key securely, using an env file!`
-);
+// console.log(
+//     `✅ Finished! We've loaded our secret key securely, using an env file!`
+// );
---
+++ reading_from_solana_blockchain.ts
@@ -0,0 +1,19 @@
+// import { Connection, clusterApiUrl } from "@solana/web3.js"
+
+// const connection = new Connection(clusterApiUrl("devnet"))
+
+// console.log('connection', connection);
+
+// console.log("Connected !")
+
+import { Connection, PublicKey, clusterApiUrl, LAMPORTS_PER_SOL } from "@solana/web3.js";
+
+const connection = new Connection(clusterApiUrl("devnet"));
+const address = new PublicKey('C8fyty8H6ZVDQyFaftmWAquFkH53xAchEM4PY9QKcWuD');
+const balance = await connection.getBalance(address);
+const balanceInSol = balance / LAMPORTS_PER_SOL;
+
+console.log(`The balance of the account at ${address} is ${balance} lamports`);
+console.log(`The balance of the account at ${address} is ${balanceInSol} sol`);
+console.log('LAMPORTS_PER_SOL', LAMPORTS_PER_SOL);
+console.log(`✅ Finished!`)
