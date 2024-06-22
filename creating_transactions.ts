6f97e60771ccc1a19661fdb65e1056b3a0ccc152
Kaustubh <kaustubhkumbhare02@gmail.com>
2024-06-22 17:41:26 +0530

    transaction of sol

---
+++ creating_transactions.ts
@@ -0,0 +1,40 @@
+import * as dotenv from 'dotenv'
+dotenv.config()
+
+import { getKeypairFromEnvironment } from '@solana-developers/helpers'
+import { Connection, SystemProgram, clusterApiUrl, PublicKey, Transaction, sendAndConfirmTransaction } from '@solana/web3.js'
+
+const senderKeypair = getKeypairFromEnvironment("SECRET_KEY");
+const receiverPublicKey = process.argv[2];
+
+const connection = new Connection(clusterApiUrl('devnet'))
+let balanceOfSender = await connection.getBalance(new PublicKey(senderKeypair.publicKey))
+let balanceOfReceiver = await connection.getBalance(new PublicKey(receiverPublicKey))
+
+console.log(`------------------------------------public addresses----------------------------------------------------------------`)
+console.log(`sender's public address : ${senderKeypair.publicKey}`)
+console.log(`receiver's public address : ${receiverPublicKey}`)
+console.log(`------------------------------------available lamports----------------------------------------------------------------`)
+console.log(`sender's balance : ${balanceOfSender}`)
+console.log(`receiver's balance : ${balanceOfReceiver}`)
+
+console.log(`------------------------------------Transaction of sol----------------------------------------------------------------`)
+const transaction = new Transaction();
+
+const lamportsToSend = 5000;
+
+const sendSolInstruction = SystemProgram.transfer({
+    toPubkey: receiverPublicKey,
+    fromPubkey: senderKeypair.publicKey,
+    lamports: lamportsToSend
+})
+
+transaction.add(sendSolInstruction);
+
+const signature = await sendAndConfirmTransaction(
+    connection,
+    transaction,
+    [senderKeypair]
+)
+
+console.log(`${lamportsToSend} lamports has been sent to the address  ${receiverPublicKey}`)
